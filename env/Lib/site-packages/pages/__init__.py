f0abb7343ec4a615084bbe37bb11c36c2161cad8
varshinidwarapureddy <73581376+varshinidwarapureddy@users.noreply.github.com>
2021-04-02 14:07:54 +0530

    initial commet

---
+++ env/Lib/site-packages/pages/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""Django page CMS module."""
+VERSION = (2, 0, 11)
+__version__ = '.'.join(map(str, VERSION))
+__author__ = "Batiste Bieler"
+__contact__ = "batiste.bieler@gmail.com"
+__homepage__ = "https://github.com/batiste/django-page-cms"
+__docformat__ = "restructuredtext"
+__doc__ = 'A tree based Django CMS application'
+__license__ = 'BSD'
+__keywords__ = ['django', 'cms']
+
+default_app_config = 'pages.app_config.BasicCmsConfig'
